25759cfb09119ecd6d26c2804d68098a2dfbdc93
DeadInsideDi <matvei778990@gmail.com>
2026-04-07 22:34:36 +0300

    added .env api url

--- src/services/api/baseApi.ts
+++ src/services/api/baseApi.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 import type { ItemQueryRequest } from '../../types'
 
 export const axiosInstance = axios.create({
-	baseURL: 'http://localhost:3001/api',
+	baseURL: import.meta.env.VITE_API_URL,
 	headers: {
 		'Content-Type': 'application/json',
 	},
